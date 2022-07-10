ad787302757ddeea7bc38c107bbc0620b435c079
deejayy <deejayy@deejayy.hu>
2022-07-10 13:09:38 +0200

    Setup store, path aliases, loading, noscript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '@env/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    StoreModule.forRoot(
+      {},
+      {
+        runtimeChecks: {
+          strictStateImmutability: false,
+        },
+      },
+    ),
+    EffectsModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      name: 'Angular 14 Bootstrap',
+      maxAge: 200,
+      logOnly: environment.production,
+    }),
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'en-US' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
