6a46abbb9df49278e0ee3c11753138d4b195f1da
deejayy <deejayy@deejayy.hu>
2022-07-20 19:40:30 +0200

    Prepare localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveConfigModule } from '@deejayy/reactive-config';
@@ -32,7 +32,6 @@ import { AppComponent } from './app.component';
       logOnly: environment.production,
     }),
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'en-US' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
